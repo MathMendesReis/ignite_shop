b2fb3d13986cb0de0c83ccb55b3aa91f1f32a789
matheus mendes <matheusmendesreis@outlook.com>
2023-11-02 15:22:16 -0300

    feat:card pronto

--- src/app/(home)/components/cardProd.tsx
+++ src/app/(home)/components/cardProd.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 interface Product {
   id: number;
@@ -13,7 +14,7 @@ export default function CardProd({
   image,
 }: Product) {
   return (
-    <div className="keen-slider__slide  shadow-[0px_0px_48px_0px_rgba(0,0,0,0.90)] rounded-lg 
+    <Link href={``} className="keen-slider__slide  shadow-[0px_0px_48px_0px_rgba(0,0,0,0.90)] rounded-lg 
     bg-[linear-gradient(180deg,_#1EA483_0%,_#7465D4_100%)] flex items-center flex-col  p-1 justify-end " >
       <Image src={'/Card2.png'} alt="" quality={100} width={520} height={480} className="m-auto" />
       <footer className="w-full h-[6.125rem] pl-8 pr-10 py-8 bg-neutral-800 bg-opacity-90 rounded-md justify-between items-center inline-flex">
@@ -25,7 +26,7 @@ export default function CardProd({
           maximumFractionDigits: 0,
         })}</p>
       </footer>
-    </div>
+    </Link>
 
   )
 }
